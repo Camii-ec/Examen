121362705b007a5910a22aba91aa8d1e2308628f
Sebastian Celaya <sebastin.celaya@uc.cl>
2022-11-27 19:39:00 -0300

    Aca va la explicacion de cada variable en general

--- Pregunta 2/script-p2-examen_eyp2435.R
+++ Pregunta 2/script-p2-examen_eyp2435.R
@@ -46,4 +46,23 @@ nombres == "numper"
 
 
 
+# Dentro de las variables más importantes tenemos las presentes en el objetivo que detallan la 
+# demografía (Urbano o Rural, región o modulo REGIDTRO HOGARES), 
+# educacion (nivel educacional o modulo de EDUCACION), 
+# salud (s12. ¿A qué sistema previsional de salud pertenece usted? o de modulo SALUD), 
+# vivienda (v1. ¿Cuál es el tipo de vivienda que ocupa el entrevistado? o modulo VIVIENDA Y ENTORNO), 
+#que trabajo desempeña(Ch1. Chequeo de situación ocupacional. o Modulo ingreso) e 
+# ingresos(ytot Ingreso Total, y totcor Ingreso total corregido).
+# 
+# De esto tenemos que analizar la 
+# pobreza (Situación de pobreza por ingresos)
+# y los grupos prioritarios (edad, r3. Pueblos indígenas, ¿pertenece usted o es descendiente de alguno de ellos?, y20e. Mes pasado Subsidio a la discapacidad mental?, r1a. ¿Cuál es la nacionalidad?)
+
+
+# FORMA BONITA PARA EL TEXTO
+
+# Dentro de las variables de interés podemos contar con las variables que buscamos conocer del onjetivo de la encuesta. Por lo tanto las variables de interés son los ingresos ganados y grupos prioritarios (estos grupo se encuentran en diversas variables de la base de datos). Estas variables de interés también nos importa conocer su relación  con la demografía, educación, salud, vivienda, trabajo e ingresos que tienen esas personas. por lo tanto estos aspectos también serán considerados como variables de interés.
+
+
+
 
