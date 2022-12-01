3cc95b3707ebeb602dc27698ab020505441102a8
Camila Echeverría <cecheverriac@uc.cl>
2022-12-01 14:54:16 -0300

    ocupando la cabeza pa no llorar JDKSJDSK

--- Pregunta 2/script-p2-examen_eyp2435.R
+++ Pregunta 2/script-p2-examen_eyp2435.R
@@ -15,40 +15,6 @@ View(datos)
 
 nombres = colnames(datos)
 
-#######
-# Objetivos
-
-# POBREZA - GRUPOS PRIORITARIOS respecto a DEMOGRAFÍA, EDUCACIÓN, SALUD, VIVIENDA, TRABAJO e INGRESOS
-
-
-# Cobestura y focalizacion de los gastos fiscales de programas sosciales nacionales según ingreso hogares y su distribución
-
-
-# Descripción de variables:
-
-# pag 15 del documento?
-
-
-# Dentro de las variables más importantes tenemos las presentes en el objetivo que detallan la 
-# demografía (Urbano o Rural, región o modulo REGIDTRO HOGARES), 
-# educacion (nivel educacional o modulo de EDUCACION), 
-# salud (s12. ¿A qué sistema previsional de salud pertenece usted? o de modulo SALUD), 
-# vivienda (v1. ¿Cuál es el tipo de vivienda que ocupa el entrevistado? o modulo VIVIENDA Y ENTORNO), 
-#que trabajo desempeña(Ch1. Chequeo de situación ocupacional. o Modulo ingreso) e 
-# ingresos(ytot Ingreso Total, y totcor Ingreso total corregido).
-# 
-# De esto tenemos que analizar la 
-# pobreza (Situación de pobreza por ingresos)
-
-# y los grupos prioritarios (edad, r3. Pueblos indígenas, ¿pertenece usted o es descendiente de alguno de ellos?, y20e. Mes pasado Subsidio a la discapacidad mental?, r1a. ¿Cuál es la nacionalidad?)
-
-# GRUPOS PRIOTITARIOS (Ch4. Chequeo de situación de dependencia)
-
-
-# FORMA BONITA PARA EL TEXTO
-
-# Dentro de las variables de interés podemos contar con las variables que buscamos conocer del onjetivo de la encuesta. Por lo tanto las variables de interés son los ingresos ganados y grupos prioritarios (estos grupo se encuentran en diversas variables de la base de datos). Estas variables de interés también nos importa conocer su relación  con la demografía, educación, salud, vivienda, trabajo e ingresos que tienen esas personas. por lo tanto estos aspectos también serán considerados como variables de interés.
-
 rm <- chilemapas::mapa_comunas %>% 
   filter(codigo_region == 13)
 
@@ -343,4 +309,91 @@ psi <- diag(rep(1,10)-L^2)
 S <- L%*%t(L)+psi
 
 corrplot::corrplot(S)
+
+
+# Regresión ---------------------------------------------------------------
+
+str(muerte)
+names(muerte)
+
+muerte <- na.omit(datos1)
+
+muerte$region <- as.factor(muerte$region)
+muerte$comuna <- as.factor(muerte$comuna)
+muerte$asiste <- as.character(muerte$asiste)
+muerte$esc <- as.factor(muerte$esc)
+muerte$educ <- as.factor(muerte$educ)
+muerte$depen <- as.factor(muerte$depen)
+muerte$activ <- as.factor(muerte$activ)
+muerte$indmat <- as.factor(muerte$indmat)
+muerte$indsan <- as.factor(muerte$indsan)
+muerte$hacinamiento <- as.factor(muerte$hacinamiento)
+muerte$pobreza <- as.factor(muerte$pobreza)
+muerte$pobreza_multi_4d <- as.factor(muerte$pobreza_multi_4d)
+muerte$pobreza_multi_5d <- as.factor(muerte$pobreza_multi_5d)
+muerte$dau <- as.factor(muerte$dau)
+muerte$dautr <- as.factor(muerte$dautr)
+muerte$iai <- as.factor(muerte$iai)
+muerte$iae <- as.factor(muerte$iae)
+
+muerte$numper <- as.numeric(muerte$numper)
+muerte$ytotcorh <- as.numeric(muerte$ytotcorh)
+
+destruccion <- fastDummies::dummy_columns(muerte)[,-c(1:3, 5:14, 16:20)]
+
+
+## Regresión con una respuesta ----
+
+pepito <- lm(ytotcorh ~., destruccion)
+summary(pepito)
+
+juanito <- step(pepito, trace = FALSE)
+datos.juanito <- summary(juanito)
+
+## Intervalo de confianza para betas
+
+alpha <- 0.05
+
+t <- qt(1-alpha/2, nrow(datos1)-1)
+
+ic.upper <- datos.juanito$coefficients[,1] + t*datos.juanito$coefficients[,2]
+ic.lower <- datos.juanito$coefficients[,1] - t*datos.juanito$coefficients[,2]
+
+## Regresión con múltiples respuestas ----
+
+respuesta.region <- function(datos){
+  y <- 1:500
+  aux <- datos %>% group_by(region)
+  
+  for(reg in 1:16){
+    y <- cbind(y, as.numeric(datos$ytotcorh[which(aux$region == as.character(reg))]))
+  }
+  
+  return(y[,-1])
+}
+
+muestra <- function(datos){
+  datitos <- matrix(1, nrow = 1, ncol = ncol(datos))
+  datos <- datos %>% group_by(region)
+  
+  for(reg in 1:16){
+    aux <- datos %>% filter(region==as.character(reg)) %>% 
+      as.matrix()
+    n <- sample(1:nrow(aux), 500)
+    datitos <- rbind(datitos, aux[n,])
+  }
+  
+  names(datitos) <- names(datos)
+  
+  return(datitos[-1,])
+}
+
+panico <- muestra(muerte)[,-1]
+
+Y <- respuesta.region(data.frame(panico))
+
+agonia <- panico[,-14]
+agonia[,3] <- as.numeric(agonia[,3])
+
+desesperacion <- fastDummies::dummy_columns(agonia, remove_first_dummy = TRUE)
 
