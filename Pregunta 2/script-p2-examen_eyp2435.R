d8f44688763c3ddce2ee2498b39cb4352c1ac56e
Camila Echeverría <cecheverriac@uc.cl>
2022-12-02 21:16:36 -0300

    creo que al fin llegué a algo que vale la pena

--- Pregunta 2/script-p2-examen_eyp2435.R
+++ Pregunta 2/script-p2-examen_eyp2435.R
@@ -419,4 +419,112 @@ mod_Pears3 <- factanal(factors = 3, covmat = cor, n.obs = nrow(yanomas))
 
 
 
+# Educación ---------------------------------------------------------------
 
+educacion <- datos %>% 
+  dplyr::select( e1:e0, -c(e4_esp, e5a_esp, e13b1_esp, e13b2_esp, e7, 
+                           e9nom, e9dir, e9com, e18a))
+
+
+mayores15 <- educacion[is.na(educacion$e1)==FALSE,]  %>% 
+  dplyr::select(-e4, -e16, -c(e14pbu:e14pbm)) 
+
+mayores15[is.na(mayores15)] <- 0 ##En las casillas, 0="no aplica"
+
+cor(mayores15)
+corrplot(cor(mayores15))
+
+# Eliminamos las correlaciones más bajas
+mayores15 <- mayores15 %>% 
+  dplyr::select(-c(e1, e2, e5a:e6b, e9com_c_cod, e9com_p_cod, e13b1, e13b2, e19_2, e0,
+                   e18b, e7_subarea:e8, e14bu:e14bpc, e15b)) %>% 
+  scale()
+
+corrplot(cor(mayores15))
+
+factanal(mayores15, factors = 2, rotation = "none")
+
+menores15 <- educacion[is.na(educacion$e1)==TRUE,] %>% 
+  dplyr::select(-e1, -e2, -c(e7_subarea:e8), -e9rbd_sup, -e9sup_g,
+                -e12e, -c(e17:e19_2))
+
+menores15[is.na(menores15)] <- 0
+
+cor.men <- cor(menores15)
+corrplot(cor(menores15))
+
+factanal(menores15, factors = 3, rotation = "none")
+
+
+# Componentes principales -------------------------------------------------
+
+educacion <- datos %>% 
+  dplyr::select( e1:e0, -c(e4_esp, e5a_esp, e13b1_esp, e13b2_esp, e7, 
+                           e9nom, e9dir, e9com))
+
+educacion[is.na(educacion)] <- 0
+
+corrplot(cor(educacion))
+
+sigma <- cor(educacion)
+
+valp <- eigen(sigma)$values
+vecp <- eigen(sigma)$vectors
+
+plot(valp/sum(valp)) #
+
+#test para 10 componentes
+
+f <- sum(valp[11:length(valp)])/sum(valp)
+var.f <- 2*(0.25/sum(diag(sigma)))^2*sum(valp[1:10]^2) + 
+  2*(0.75/sum(diag(sigma)))^2*sum(valp[11:length(valp)]^2)
+  
+sqrt(length(educacion)-1)*abs(f-0.25) < qnorm(0.95)*sqrt(var.f)
+
+# Rechazamos H0, pedemos ignorar las últimas componentes
+
+valp <- eigen(sigma)$values[1:10]
+vecp <- eigen(sigma)$vectors[,1:10]
+
+prop.var <- valp/sum(valp)  
+
+
+# Intento número 1000 -----------------------------------------------------
+
+datos1[is.na(datos1)] <- 0
+
+sigma <- cov(scale(datos1))
+
+valp <- eigen(sigma)$values
+vecp <- eigen(sigma)$vectors
+
+plot(valp/sum(valp))
+
+#test para 8 componentes
+f <- sum(valp[9:length(valp)])/sum(valp)
+var.f <- 2*(0.25/sum(diag(sigma)))^2*sum(valp[1:8]^2) + 
+  2*(0.75/sum(diag(sigma)))^2*sum(valp[9:length(valp)]^2)
+
+sqrt(length(valp)-1)*abs(f-0.25) < qnorm(0.975)*sqrt(var.f)
+#rechazamos H0, podemos usar 8 componentes 
+
+valp <- eigen(sigma)$values[1:8]
+vecp <- eigen(sigma)$vectors[,1:8]
+
+prop.var <- valp/sum(valp)
+
+plot(prop.var)
+
+cumsum(prop.var)
+
+correlaciones <- function(valp, vecp, sigma){
+  r <- matrix(1, nrow=17)
+  for(i in 1:8){
+    r <- cbind(r, vecp[,i]*sqrt(valp[i]/sigma[i,i]))
+  }
+  
+  return(r[,-1])
+}
+
+r <- correlaciones(valp, vecp, sigma)
+rowSums(r^2)
